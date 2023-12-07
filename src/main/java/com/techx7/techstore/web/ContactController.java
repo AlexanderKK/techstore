0f13f8b903cae85da9ecb13f8e5b5895f526b696
Alex <alexander.k.kostadinov@gmail.com>
2023-12-07 17:45:28 +0200

    Updated project

--- src/main/java/com/techx7/techstore/web/ContactController.java
+++ src/main/java/com/techx7/techstore/web/ContactController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:8080", "https://techx7.yellowflower-41c8e8d4.westeurope.azurecontainerapps.io"})
 @Controller
 public class ContactController {
 
